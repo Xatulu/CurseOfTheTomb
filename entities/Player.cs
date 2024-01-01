708f0d3350635e5aea2338a8ea7fc9e0935e6ec0
Xatulu <patrick.schreck@me.com>
2024-01-01 13:14:51 +0100

    Added Animations

--- entities/Player.cs
+++ entities/Player.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Player : CharacterBody2D
 {
     [Export]
-    private float speed = 600.0f;
+    private float speed = 1000.0f;
     [Export]
     private float gravity = (float)ProjectSettings.GetSetting("physics/2d/default_gravity");
     [Export]
@@ -13,11 +13,14 @@ public partial class Player : CharacterBody2D
     private float jump_hang_time = 0.15f;
 
     private Node2D pivot;
+    private AnimationPlayer animPlayer;
+
     private float hang_time_remaining = 0.0f;
 
     public override void _Ready()
     {
         pivot = GetNode<Node2D>("Pivot");
+        animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
     }
     public override void _PhysicsProcess(double delta)
     {
@@ -43,15 +46,33 @@ public partial class Player : CharacterBody2D
             else
             {
                 newVelocity.Y += gravity;
+                if (newVelocity.Y > 250)
+                {
+                    animPlayer.Play("fall");
+                }
             }
 
         }
 
-        if (IsOnFloor() && Input.IsActionJustPressed("jump"))
+        if (IsOnFloor())
         {
-            newVelocity.Y = jump_velocity;
-            hang_time_remaining = jump_hang_time;
+            if (Input.IsActionJustPressed("jump"))
+            {
+                newVelocity.Y = jump_velocity;
+                hang_time_remaining = jump_hang_time;
+                animPlayer.Play("jump");
+            }
+            else if (Velocity.X != 0.0)
+            {
+                animPlayer.Play("run");
+            }
+            else
+            {
+                animPlayer.Play("idle");
+            }
         }
+
+
 
         Velocity = newVelocity;
         MoveAndSlide();
